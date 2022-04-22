91efe85cd7cd6d906c8ec9031dac35631ce51277
Berfin <berfin@minerva.kgi.edu>
2022-04-22 16:19:29 +0300

    doneeee

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching (drivers, string) {
+    
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch (drivers, string) {
+    
+    return drivers.filter(driver => driver[0] === string[0]);
+}
+
+function matchName (array, string) {
+    
+    return array.filter(driver => driver.name === string);
+}
